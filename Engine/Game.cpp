6684daa1df9e979d67d9c2c41fc8cb0e0913a071
chili <ray_kun@hotmail.com>
2018-05-30 11:20:19 +0900

    translation

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -47,5 +47,12 @@ void Game::ComposeFrame()
 	{
 		gfx.DrawLine( { 100.0f,100.0f },(Vec2)wnd.mouse.GetPos(),Colors::Yellow );
 	}
-	gfx.DrawClosedPolyline( Star::Make( 150.0f,75.0f ),Colors::Red );
+
+	auto poly = Star::Make( 150.0f,75.0f,8 );
+	for( auto& v : poly )
+	{
+		v += Vec2{ 200.0f,200.0f };
+	}
+
+	gfx.DrawClosedPolyline( poly,Colors::Red );
 }
